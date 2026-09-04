7399f3848ac4d1ae2276dda838faa5be1825a724
Karmegam <karmegam.seerangan@syncfusion.com>
2026-09-04 11:52:41 +0530

    Added the Container tested application

---
+++ ContainerAppTest/BasePathWasmContainerApp/BasePathWasmContainerApp/BasePathWasmContainerApp/Program.cs
@@ -0,0 +1,34 @@
+using BasePathWasmContainerApp.Client.Pages;
+using BasePathWasmContainerApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddHttpContextAccessor();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedPrefix;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+var app = builder.Build();
+app.UseForwardedHeaders();  
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BasePathWasmContainerApp.Client._Imports).Assembly);
+
+app.Run();
